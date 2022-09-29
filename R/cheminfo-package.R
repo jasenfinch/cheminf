c4b040f4919cf1c8b7dec953cb7a4a262feec517
jasenfinch <jsf9@aber.ac.uk>
2022-09-29 20:26:16 +0100

    generated documentation

---
+++ R/cheminfo-package.R
@@ -0,0 +1,4 @@
+## usethis namespace: start
+#' @useDynLib cheminf, .registration = TRUE
+## usethis namespace: end
+NULL
--- R/metabolite_database.R
+++ R/metabolite_database.R
@@ -1,3 +1,4 @@
+#' @importFrom tibble tibble
 
 setClass(
   'MetaboliteDatabase',

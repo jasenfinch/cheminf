8c15b33bb1b130740f230ecd21f8b4c3ad6bde9b
jasenfinch <jsf9@aber.ac.uk>
2022-12-19 21:02:39 +0000

    remove remaining C++ infrastructure

--- R/cheminf-package.R
+++ R/cheminf-package.R
@@ -2,8 +2,3 @@
 globalVariables(c('Negative_Charge','Positive_Charge','MF','Accurate_Mass','Total_Charge','HBA1',
                   'TPSA','NHH','COO','Name','Rule','m/z','Possible','ID','.','Measured m/z',
                   'Theoretical m/z','SMILES'))
-
-## usethis namespace: start
-#' @useDynLib cheminf, .registration = TRUE
-## usethis namespace: end
-NULL

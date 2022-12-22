c6c3192aacefc0f2f48dbdbc1adaf3432b26ff6f
jasenfinch <jsf9@aber.ac.uk>
2022-12-22 18:48:42 +0000

    allow chemicalDescriptors to return a tibble if argument `SMILES` is length 0

--- R/descriptors.R
+++ R/descriptors.R
@@ -30,14 +30,33 @@ smartsSearch <- function(SMILES,SMARTS){
 
 chemicalDescriptors <- function(SMILES){
   
-  desc <- c('HBA1',
-            'HBA2',
-            'HBD',
-            'logP',
-            'TPSA'
-  )
-  
-  descs <- SMILES %>% 
+  if (length(SMILES) == 0){
+    desc <- tibble(
+      SMILES = character(),
+      MF = character(),
+      Accurate_Mass = numeric(),
+      HBA1 = numeric(),
+      HBA2 = numeric(),
+      HBD = numeric(),
+      logP = numeric(),
+      TPSA = numeric(),
+      Negative_Charge = numeric(),
+      Positive_Charge = numeric(),
+      NHH = numeric(),
+      OH = numeric(),
+      COOH = numeric(),
+      COO = numeric(),
+      Total_Charge = numeric()
+    )
+  } else {
+    desc <- c('HBA1',
+              'HBA2',
+              'HBD',
+              'logP',
+              'TPSA'
+    )
+    
+    descs <- SMILES %>% 
       future_map_dfr(~{
         molRefs = forEachMol("SMILES",.x,identity)
         
@@ -47,48 +66,50 @@ chemicalDescriptors <- function(SMILES){
           mutate(MF = smilesToMF(.x))
       },
       .options = furrr_options(seed = TRUE)) %>% 
-    mutate(SMILES = SMILES) %>% 
-    relocate(SMILES:MF,.before = 'HBA1')
-  
-  Fgroups <- tibble(Name = c('Negative_Charge',
-                             'Positive_Charge',
-                             'NHH',
-                             'OH',
-                             'COOH',
-                             'COO'),
-                    String = c('[-]',
-                               '[+]',
-                               "[NX3;H2]",
-                               "[OX2H]",
-                               "[CX3](=O)[OX2H1]",
-                               "[CX3](=O)[OX1H0-]")
-  )
-  
-  
-  groups <- Fgroups %>%
-    split(1:nrow(Fgroups)) %>%
-    map(~{
-      string <- .$String
-      g <- future_map_int(SMILES,~{
-        s <- .
-        s %>%
-          smartsSearch(string)
-      },
-      .options = furrr_options(seed = TRUE)) %>%
-        as_tibble()
-      names(g) <- .$Name
-      return(g)
-    }) %>%
-    bind_cols()
-  
-  desc <- bind_cols(descs,groups) %>%
-    mutate(Total_Charge = -Negative_Charge + Positive_Charge,
-           MF = future_map_chr(SMILES,smilesToMF,
-                               .options = furrr_options(seed = TRUE)),
-           `Accurate_Mass` = future_map_dbl(SMILES,smilesToAccurateMass,
-                                            .options = furrr_options(seed = TRUE)) %>% round(5)
-           ) %>% 
-    relocate(Accurate_Mass,.after = 'MF')
+      mutate(SMILES = SMILES) %>% 
+      relocate(SMILES:MF,.before = 'HBA1')
+    
+    Fgroups <- tibble(Name = c('Negative_Charge',
+                               'Positive_Charge',
+                               'NHH',
+                               'OH',
+                               'COOH',
+                               'COO'),
+                      String = c('[-]',
+                                 '[+]',
+                                 "[NX3;H2]",
+                                 "[OX2H]",
+                                 "[CX3](=O)[OX2H1]",
+                                 "[CX3](=O)[OX1H0-]")
+    )
+    
+    
+    groups <- Fgroups %>%
+      split(1:nrow(Fgroups)) %>%
+      map(~{
+        string <- .$String
+        g <- future_map_int(SMILES,~{
+          s <- .
+          s %>%
+            smartsSearch(string)
+        },
+        .options = furrr_options(seed = TRUE)) %>%
+          as_tibble()
+        names(g) <- .$Name
+        return(g)
+      }) %>%
+      bind_cols()
+    
+    desc <- bind_cols(descs,groups) %>%
+      mutate(Total_Charge = -Negative_Charge + Positive_Charge,
+             MF = future_map_chr(SMILES,smilesToMF,
+                                 .options = furrr_options(seed = TRUE)),
+             `Accurate_Mass` = future_map_dbl(SMILES,smilesToAccurateMass,
+                                              .options = furrr_options(seed = TRUE)) %>% round(5)
+      ) %>% 
+      relocate(Accurate_Mass,.after = 'MF')
+    
+  }
   
   return(desc)
 }

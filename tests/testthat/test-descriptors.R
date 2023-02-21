4f2b93a58c4493ceca0d7463e21bbe4c67b7ab3f
jasenfinch <jsf9@aber.ac.uk>
2023-02-21 09:33:08 +0000

    full test coverage

--- tests/testthat/test-descriptors.R
+++ tests/testthat/test-descriptors.R
@@ -2,3 +2,8 @@
 test_that('smartsSearch errors if > 1 SMILES specified',{
   expect_error(smartsSearch(amino_acids$SMILES,"[OX2H]"))
 })
+
+
+test_that('chemicalDescriptors works for empty SMILES',{
+  expect_equal(nrow(chemicalDescriptors(character())),0)
+})
